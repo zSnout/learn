fe14e53a115bfc62f21a8aba7bdc7157928864e2
Zachary <zsakowitz@gmail.com>
2024-08-01 04:44:33 +0000

    apple is so annoying

--- src/el/QuizLayout.tsx
+++ src/el/QuizLayout.tsx
@@ -158,14 +158,11 @@ export function ResponseGray(props: {
 }) {
   return (
     <button
-      class="hover:border-z hover:focus:border-z-focus hover:bg-z-body bg-z-body-selected z-field relative h-12 w-full rounded border-transparent px-0 py-1 shadow-none"
+      class="hover:border-z hover:focus:border-z-focus hover:bg-z-body bg-z-body-selected z-field relative h-12 w-full cursor-pointer appearance-auto rounded border-transparent px-0 py-1 shadow-none"
       onClick={() => {
         props.onClick?.()
       }}
-      ontouchstart={() => {}}
-      ontouchend={() => {}}
-      ontouchcancel={() => {}}
-      ontouchmove={() => {}}
+      role="button"
     >
       {props.children}
     </button>
@@ -179,8 +176,13 @@ export function Response(props: {
 }) {
   return (
     <button
-      class={"relative rounded py-1 " + props.class}
-      onClick={() => props.onClick?.()}
+      class={
+        "relative cursor-pointer appearance-auto rounded py-1 " + props.class
+      }
+      onClick={() => {
+        props.onClick?.()
+      }}
+      role="button"
     >
       {props.children}
     </button>
